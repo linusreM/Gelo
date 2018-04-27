85afc204c5863dd0053d11319c02fae9195b7561
Jacob Linder <jacobolinder@gmail.com>
2018-04-27 14:43:42 +0200

    updated aruco test scripts

--- camera/aruco_many.py
+++ camera/aruco_many.py
@@ -81,42 +81,4 @@ while True:
 	except:
 		print "no code"
 
-img = vs.readUndistorted()
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #EVENTUELLT
-
-corners, ids, rejected = aruco.detectMarkers(gray, dictionary, parameters = parameters)
-rvecs, tvecs, obj = aruco.estimatePoseSingleMarkers(corners, 65.18, vs.mtx, vs.dist)
-
-try:
-	if (rvecs.size > 0):
-
-		for marker in tvecs:
-			tvecs_x = marker[0,0]
-			tvecs_y = marker[0,1]
-			tvecs_z = marker[0,2]
-
-		for marker in rvecs:
-			#print marker
-			rvecs_x = marker[0,0]
-			rvecs_y = marker[0,1]
-			rvecs_z = marker[0,2]
-
-		msg = "{}#{}#{}#{}#{}#{}#{}#{}#{}$".format(
-			id, 
-			type, 
-			"data", 
-			tvecs_x, 
-			tvecs_y, 
-			tvecs_z, 
-			rvecs_x, 
-			rvecs_y, 
-			rvecs_z
-		) 
-		
-		print msg
-		clientsocket.send(msg)
-
-except:
-	print "no code"
-
 vs.stop()
