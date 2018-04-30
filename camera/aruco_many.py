29bfa99e53cdce5922158cfbf69150e677166c34
Jacob Linder <jacobolinder@gmail.com>
2018-04-30 12:54:23 +0200

    Changes to be committed: 	Massa kamera grejjor

--- camera/aruco_many.py
+++ camera/aruco_many.py
@@ -10,7 +10,7 @@ import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print('attempting connect\n')
-clientsocket.connect(('localhost', int(sys.argv[3])))
+clientsocket.connect(('130.229.137.36', int(sys.argv[3])))
 
 print('Connection open\n')
 #clientsocket.send('Connection open\n')
@@ -42,43 +42,43 @@ parameters = aruco.DetectorParameters_create()
 
 parameters.cornerRefinementMethod = 1
 parameters.cornerRefinementWinSize = 5
-parameters.cornerRefinementMaxIterations = 30 
+parameters.cornerRefinementMaxIterations = 30
 parameters.cornerRefinementMinAccuracy = 0.0001
 
 vs = VideoStream(isPiCamera = isPiCamera, resolution = resolution).start()
 time.sleep(2.0)
 
 while True:
-	img = vs.readUndistorted()
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #EVENTUELLT
+    img = vs.readUndistorted()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #EVENTUELLT
 
-	corners, ids, rejected = aruco.detectMarkers(gray, dictionary, parameters = parameters)
-	rvecs, tvecs, obj = aruco.estimatePoseSingleMarkers(corners, 65.18, vs.mtx, vs.dist)
+    corners, ids, rejected = aruco.detectMarkers(gray, dictionary, parameters = parameters)
+    rvecs, tvecs, obj = aruco.estimatePoseSingleMarkers(corners, 65.18, vs.mtx, vs.dist)
 
-	markers = []
+    markers = []
 
-	try:
-		for i in range(len(ids)):
-			marker = Marker(corners[i], ids[i], tvecs[i], rvecs[i])
-			markers.append(marker)
+    try:
+        for i in range(len(ids)):
+            marker = Marker(corners[i], ids[i], tvecs[i], rvecs[i])
+            markers.append(marker)
 
 
-		for marker in markers:
-			msg = "{}#{}#{}#{}#{}#{}#{}#{}#{}$".format(
-					id, 
-					type, 
-					marker.data, 
-					marker.tvecs_x, 
-					marker.tvecs_y, 
-					marker.tvecs_z, 
-					marker.rvecs_x, 
-					marker.rvecs_y, 
-					marker.rvecs_z
-			) 	
-			print msg
-			clientsocket.send(msg)
-		time.sleep(3.0)
-	except:
-		print "no code"
+        for marker in markers:
+            msg = "{}#{}#{}#{}#{}#{}#{}#{}#{}$".format(
+                    id,
+                    type,
+                    marker.data,
+                    marker.tvecs_x,
+                    marker.tvecs_y,
+                    marker.tvecs_z,
+                    marker.rvecs_x,
+                    marker.rvecs_y,
+                    marker.rvecs_z
+            )
+            print msg
+            clientsocket.send(msg)
+        time.sleep(3.0)
+    except:
+        print "no code"
 
 vs.stop()
